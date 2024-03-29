2bbbb2fb752e8731018b607fe41c996200ec51c4
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-03-29 12:24:33 -0700

    Added Home landing section
    Landing section has large name and center icon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main>
       <Header page="home"></Header>
+      <section className='landing-section'>
+        <h1><p>E</p><p>T</p><p className='text-background'>H</p><p>A</p><p>N</p></h1>
+        <img src="/images/main-shape.svg" alt="main landing shape" height={175} width={175} className='main-shape'/>
+      </section>
     </main>
   );
 }
