a4b9961165901286003dfcfb02a7b4e799875a20
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-04-05 23:50:40 -0700

    heatmap/social media update

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -11,103 +11,75 @@ const SocialMedia = () => {
     gsap.registerPlugin(useGSAP)
     // Event Listeners for the shape behind the social media logos
     // INSTAGRAM
-    const handleMouseEnter1 = (e) => {
-        const mouseX = container1.current.getBoundingClientRect().x
-        const mouseY = container1.current.getBoundingClientRect().y
+    const handleMouseEnter1 = () => {
         gsap.timeline()
-        .to('#shape-1', {
-            x: `${e.clientX - mouseX - (35/2)}`,
-            y: `${e.clientY - mouseY - (35/2)}`,
-            duration: 0.00001
-        })
-        .to('#shape-1', {
-            width: 140,
-            height: 140,
+        .to('#image1', {
             ease: "circ.inOut",
-            duration: 0.25
+            duration: 0.25,
+            width: '45px',
+            backgroundColor: '#1b998b'
         })
     };
 
     const handleMouseLeave1 = () => {
         gsap.timeline()
-        .to('#shape-1', {
-            width: 0,
-            height: 0,
+        .to('#image1', {
             ease: "circ.inOut",
-            duration: 0.5
+            duration: 0.25,
+            width: '40px',
+            backgroundColor: 'white'
         })
     };
     // GITHUB
     const handleMouseEnter2 = (e) => {
-        const mouseX = container2.current.getBoundingClientRect().x
-        const mouseY = container2.current.getBoundingClientRect().y
         gsap.timeline()
-        .to('#shape-2', {
-            x: `${e.clientX - mouseX - (35/2)}`,
-            y: `${e.clientY - mouseY - (35/2)}`,
-            duration: 0.00001
-        })
-        .to('#shape-2', {
-            width: 140,
-            height: 140,
+        .to('#image2', {
             ease: "circ.inOut",
-            duration: 0.5
+            duration: 0.25,
+            width: '45px'
         })
     };
 
     const handleMouseLeave2 = () => {
         gsap.timeline()
-        .to('#shape-2', {
-            width: 0,
-            height: 0,
+        .to('#image2', {
             ease: "circ.inOut",
-            duration: 0.25
+            duration: 0.25,
+            width: '40px'
         })
     };
 
     // LINKED IN
     const handleMouseEnter3 = (e) => {
-        const mouseX = container3.current.getBoundingClientRect().x
-        const mouseY = container3.current.getBoundingClientRect().y
-        gsap.timeline()
-        .to('#shape-3', {
-            x: `${e.clientX - mouseX - (35/2)}`,
-            y: `${e.clientY - mouseY - (35/2)}`,
-            duration: 0.00001
-        })
-        .to('#shape-3', {
-            width: 140,
-            height: 140,
+       gsap.timeline()
+        .to('#image3', {
             ease: "circ.inOut",
-            duration: 0.5
+            duration: 0.25,
+            width: '45px'
         })
     };
 
     const handleMouseLeave3 = () => {
         gsap.timeline()
-        .to('#shape-3', {
-            width: 0,
-            height: 0,
+        .to('#image3', {
             ease: "circ.inOut",
-            duration: 0.25
+            duration: 0.25,
+            width: '40px',
         })
     };
   
 
   return (
     <div className='social-media-container'>
-        <div onMouseEnter={handleMouseEnter1} onMouseLeave={handleMouseLeave1} ref={container1}>
-            <Image src='/images/instagram.svg' width={40} height={40} alt='Instagram logo'/>
-            <div id='shape-1' className='shape'></div>
+        <div onMouseEnter={handleMouseEnter1} onMouseLeave={handleMouseLeave1}>
+            <div id='image1'></div>
         </div>
         
         <div onMouseEnter={handleMouseEnter2} onMouseLeave={handleMouseLeave2} ref={container2}>
-            <Image src='/images/github.svg' width={40} height={40} alt='Github logo'/>
-            <div id='shape-2' className='shape'></div>
+            <Image src='/images/github.svg' width={40} height={40} alt='Github logo' id='image2'/>
         </div>
         <div onMouseEnter={handleMouseEnter3} onMouseLeave={handleMouseLeave3} ref={container3}>
-            <Image src='/images/linkedin.svg' width={40} height={40} alt='LinkedIn logo'/>
-            <div id='shape-3' className='shape'></div>
+            <Image src='/images/linkedin.svg' width={40} height={40} alt='LinkedIn logo' id='image3'/>
         </div>
     </div>
   )
