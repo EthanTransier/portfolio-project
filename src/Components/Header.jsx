c8de1ae55bfd6f850d58d6ea3bb1a927992515ae
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-04-19 17:22:47 -0700

    Update Header capitalization to fix netlify issue

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import Navbar from './Navbar'
 import {useState} from 'react'
-const header = (page) => {
+const Header = (page) => {
   const [navState, setNavState] = useState(false)
   // controls whether the mobile nav is open or closed
   function openNav(){
@@ -21,4 +21,4 @@ const header = (page) => {
   )
 }
 
-export default header
+export default Header
